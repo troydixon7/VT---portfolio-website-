b2963420c9282ee55f53c2bdc0fd3010e975f21d
Troy Dixon <troydixon@Troys-Mac-Pro.attlocal.net>
2022-04-04 13:01:38 -0700

    added jstoggle.js

---
+++ Project500/jstoggle.js
@@ -0,0 +1,9 @@
+/* Toggle Animations */
+const jstoggle = document.getElementById('js-toggle');
+jstoggle.addEventListener('click', () => {
+  const animations = document.querySelectorAll('[data-animation');
+  animations.forEach(animation => {
+    const running = animation.style.animationPlayState || 'running';
+    animation.style.animationPlayState = running === 'running' ? 'paused' : 'running';
+  })
+});
